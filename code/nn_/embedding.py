5e9e1226faebea0a3b0d0fb53e4ce8eb90517bcc
zuvale <alex.zubov99@googlemail.com>
2026-04-25 14:46:08 +0200

    ✨ added continuous time embedding

--- code/nn_/embedding.py
+++ code/nn_/embedding.py
@@ -1,19 +1,24 @@
+## TO-DO:
+##  - combine the two sinusoidal PE classes into one
 import torch
 import torch.nn as nn
 
 
-class SinusoidalPE(nn.Module):
+class DiscreteSinusoidalPE(nn.Module):
     """
     Implementation taken from
     https://medium.com/@hirok4/understanding-transformer-sinusoidal-position-embedding-7cbaaf3b9f6a
     """
-    def __init__(self, embedding_dim: int, timesteps: int) -> None:
+    def __init__(
+        self, embedding_dim: int, timesteps: int, max_period: float=10000.0
+    ) -> None:
         super().__init__()
 
         time_axis = torch.arange(timesteps).view(timesteps, 1)
         emb_axis = torch.arange(embedding_dim).view(1, embedding_dim)
 
-        angle_rates = 1 / torch.pow(10000, (2 * (emb_axis // 2)) / embedding_dim)
+        angle_rates = 1 / torch.pow(
+            torch.tensor(max_period), (2 * (emb_axis // 2)) / embedding_dim)
         angle_rads = time_axis * angle_rates
 
         pos_enc = torch.zeros_like(angle_rads)
@@ -24,6 +29,30 @@ class SinusoidalPE(nn.Module):
     
     def forward(self, t: torch.Tensor) -> torch.Tensor:
         return self.pos_enc[t]
+
+class ContinuousSinusoidalPE(nn.Module):
+    def __init__(
+        self, embedding_dim: int, max_period: float=10000.0,
+        time_scale: float=1.0
+    ) -> None:
+        super().__init__()
+
+        emb_axis = torch.arange(embedding_dim).view(1, embedding_dim)
+        angle_rates = 1 / torch.pow(
+            torch.tensor(max_period), (2 * (emb_axis // 2)) / embedding_dim)
+        
+        self.register_buffer("angle_rates", angle_rates)
+        self.time_scale = time_scale
+    
+    def forward(self, t: torch.Tensor) -> torch.Tensor:
+        t = t.float().view(t.size(0), 1)
+        angle_rads = self.time_scale * t * self.angle_rates
+
+        pos_enc = torch.zeros_like(angle_rads)
+        pos_enc[:, 0::2] = torch.sin(angle_rads[:, 0::2])
+        pos_enc[:, 1::2] = torch.cos(angle_rads[:, 1::2])
+
+        return pos_enc
 
 class ClassEmbedding(nn.Module):
     def __init__(
